a620c737f82f3706999920c2dccce58868f0287d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-03-10 20:47:44 +0800

    feat: add footer component

---
+++ portfolio-ui/src/components/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+const copyrightYear: number = new Date().getFullYear()
+
+export default function Footer() {
+    return (
+        <footer className="col-start-1 row-start-8 col-span-5 row-span-1 text-center">
+            <span className="text-xs justify-center">
+                &copy; {copyrightYear} Raven Carlos T. Rodrigo
+            </span>
+        </footer>
+    )
+};
