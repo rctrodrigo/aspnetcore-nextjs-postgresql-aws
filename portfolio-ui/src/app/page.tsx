bdc04b31a5757709a09f666f22b23db7c4761f08
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-03-12 22:05:40 +0800

    feat: add tools section component

--- portfolio-ui/src/app/page.tsx
+++ portfolio-ui/src/app/page.tsx
@@ -2,13 +2,15 @@ import Footer from "@/components/layouts/Footer";
 import Navbar from "@/components/layouts/Navbar/Navbar";
 import Image from "next/image";
 import About from "@/components/layouts/About";
+import Tools from "@/components/layouts/Tools";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <div className="grid grid-cols-5 grid-rows-1 gap-1">
-        <About />
+          <About />
+          <Tools />
       </div>
       <Footer />
     </>
